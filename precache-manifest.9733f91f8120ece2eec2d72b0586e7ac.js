2405a6a6c5526fbc104e14e7db1decd223c5333b
Кирилл <kirill@Mac-mini-OrangeSoft.local>
2019-01-03 14:14:50 +0300

    Updates

---
+++ precache-manifest.9733f91f8120ece2eec2d72b0586e7ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9b246381a5d4eb3cf91",
+    "url": "/static/js/main.f9b24638.chunk.js"
+  },
+  {
+    "revision": "446a14a1771d84fc1341",
+    "url": "/static/js/1.446a14a1.chunk.js"
+  },
+  {
+    "revision": "f9b246381a5d4eb3cf91",
+    "url": "/static/css/main.fc5ce0ae.chunk.css"
+  },
+  {
+    "revision": "14597115663cb6b40d25e01f6baf0d33",
+    "url": "/index.html"
+  }
+];
