c9b5f1114ef4e0dfc6e2a3fc01f9b947a817be18
Кирилл <kirill@Mac-mini-OrangeSoft.local>
2019-01-28 17:19:57 +0300

    Added https server

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const app = express();
+
+const credentials = {
+    key: fs.readFileSync('./https/key.pem', 'utf8'),
+    cert: fs.readFileSync('./https/cert.pem', 'utf8')
+};
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3016);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(3016);
+httpsServer.listen(3017);
