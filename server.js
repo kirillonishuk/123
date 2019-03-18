dd9705c1251f0512a7d77c3eb2a2acea2a56e7bb
Kirill Onishuk <kirill.onishuk@orangesoft.by>
2019-03-18 19:57:36 +0300

    Added sending of the favicon from server

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const credentials = {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/favicon.ico', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'favicon.ico'));
+});
+
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
